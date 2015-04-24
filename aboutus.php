f680f0153e9e7d2b013a878cd8ed31bf9271a19d
Hector Rincon <hecerinc@hotmail.com>
2015-04-24 18:48:53 -0500

    Modified layout of aboutus

--- aboutus.php
+++ aboutus.php
@@ -4,30 +4,28 @@
 	<div class="container aboutus">
 		<h2>Qui&eacute;nes somos</h2>
 		<div class="row">
-			<div class="six columns"><img src="img/aboutus.png" alt="About Us"></div>
-			<div class="six columns">
-				<p><strong>REALTY WORLD INCO-VALLE</strong> est&aacute; considerada entre las principales l&iacute;deres de Bienes Ra&iacute;ces en M&eacute;xico. <br><br>
-				<strong>Somos la &uacute;nica compa&ntilde;&iacute;a que incluye las 5 especialidades:</strong>
-				</p>
-				<ul>
-					<li>Comercial</li>
-					<li>Industrial</li>
-					<li>Residencial</li>
-					<li>Tur&iacute;stico</li>
-					<li>Agropecuario</li>
-				</ul>
-				<p>
-				<br>
-				Hemos desarrollado la mejor tecnología, herramientas de mercado y personal muy profesional y bien capacitado para atender sus necesidades. 
-				<br><br>
-				Jorge Paredes Guerra, Presidente y Director General, cuenta con más de 25 años de experiencia en el sector de Bienes Raíces.
-				<br><br>
-				Nuestros Gerentes altamente calificados así como el grupo de profesionales inmobiliarios se encuentran bien integrados siendo todos parte de un gran equipo de trabajo.
-				<br><br>
-				Soluciones inmobiliarias para cada necesidad garantizan un servicio y desempeño de primera clase.
-				</p>
+			<img src="img/aboutus.png" class="aboutus-image" alt="About Us">
+			<p class="main-copy"><strong>REALTY WORLD INCO-VALLE</strong> est&aacute; considerada entre las principales l&iacute;deres de Bienes Ra&iacute;ces en M&eacute;xico. <br><br>
+			<strong>Somos la &uacute;nica compa&ntilde;&iacute;a que incluye las 5 especialidades:</strong>
+			</p>
+			<ul>
+				<li>Comercial</li>
+				<li>Industrial</li>
+				<li>Residencial</li>
+				<li>Tur&iacute;stico</li>
+				<li>Agropecuario</li>
+			</ul>
+			<p>
+			<br>
+			Hemos desarrollado la mejor tecnología, herramientas de mercado y personal muy profesional y bien capacitado para atender sus necesidades. 
+			<br><br>
+			Jorge Paredes Guerra, Presidente y Director General, cuenta con más de 25 años de experiencia en el sector de Bienes Raíces.
+			<br><br>
+			Nuestros Gerentes altamente calificados así como el grupo de profesionales inmobiliarios se encuentran bien integrados siendo todos parte de un gran equipo de trabajo.
+			<br><br>
+			Soluciones inmobiliarias para cada necesidad garantizan un servicio y desempeño de primera clase.
+			</p>
 
-			</div>
 		</div>
 	</div>
 	<div class="clear h100px"></div>
